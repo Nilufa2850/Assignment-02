a4378865165f4a5c57afeffece79faa3c44c3692
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-20 22:08:07 +0530

    Question-02.C

---
+++ Question-02.C
@@ -0,0 +1,12 @@
+//Write a program to print a given number without its last digit.
+
+#include<stdio.h>
+int main()
+{
+    int x, y ;
+    printf("Enter a number : ");
+    scanf("%d",&x);
+    y=x/10;
+    printf("Unit digit of %d is %d",x,y);
+    return 0;
+}
