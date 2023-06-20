c9974a79c5e3d4ce3139bc9cecb5d2d6a8df3d97
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-20 22:40:55 +0530

    Question-06.C

---
+++ Question-06.C
@@ -0,0 +1,10 @@
+//Write a program which takes a character as an input and displays its ASCII code.
+#include<stdio.h>
+int main()
+{
+    char x ;
+    printf("Enter a character : ");
+    scanf("%c",&x);
+    printf("\nASCII Code of %c is %d",x,x);
+    return 0;
+}
