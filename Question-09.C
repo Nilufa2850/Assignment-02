18e66870ab860995fc7a0a7e2e93307116a92a25
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 09:02:44 +0530

    Question-09.C

---
+++ Question-09.C
@@ -0,0 +1,13 @@
+/*Write a program to print size of an int, a float, a char and a double type variable.*/
+
+#include<stdio.h>
+int main()
+{
+    
+    int x ;
+    float y ;
+    x = sizeof(y);
+    printf("Size is %d",x);
+
+    return 0;
+}
